0578bc5c85083383c2cf825fafcf1268dae51632
Zhi Pi <zhi1@cs.ualberta.ca>
2023-01-12 22:59:52 -0700

    update the Script.py

--- Script.py
+++ Script.py
@@ -7,3 +7,7 @@ response = requests.get("http://google.com")
 
 print(response.content)
 
+get_script = requests.get("https://raw.githubusercontent.com/PAULbigBA/CMPUT404_Lab1/main/Script.py")
+
+print(get_script.text)
+
